c88dae3508ab8a5948966ff77f81e3409098a2fb
menoRashid <mhnrashid@gmail.com>
2017-11-17 17:00:24 -0800

    added h5py stuff and did alexnet finetuning

--- code/data_processors/preprocess_horse.py
+++ code/data_processors/preprocess_horse.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.append('./')
-sys.path.append('../')
 print sys.path
 import os
 from helpers import util, visualize
@@ -9,6 +8,48 @@ import numpy as np
 import math
 import h5py
 import scipy.misc
+
+
+def make_train_test_splits_horse_based():
+	data_dir_meta ='../data/horse_51'
+	anno_file = os.path.join(data_dir_meta,'Painscore_expert_51images.csv')
+	
+	im_dir = data_dir_meta
+
+	out_dir_split = os.path.join(data_dir_meta,'train_test_split_horse_based')
+	util.mkdir(out_dir_split)
+	
+
+	anno = util.readLinesFromFile(anno_file)
+
+	anno = anno[1:]
+	anno = [[int(anno_curr.split(',')[val]) for val in [0,1,-2,-1]] for anno_curr in anno]
+	anno = np.array(anno)
+
+	exclude_idx = anno[:,-1]!=anno[:,-2]
+	print np.sum(exclude_idx)
+
+	kept_files = anno[~exclude_idx,:]
+	horse_ids = np.unique(kept_files[:,1])
+	num_splits = len(horse_ids)
+	
+	for split_num in range(num_splits):
+		arr_idx = [idx for idx in range(num_splits) if idx!=split_num]
+		horse_ids_kept = horse_ids[arr_idx]
+
+		idx_train = np.where(np.isin(kept_files[:,1],horse_ids_kept))[0]
+
+		# np.concatenate([pos_idx_split[val] for val in arr_idx]+[neg_idx_split[val] for val in arr_idx])
+		idx_test = np.where(kept_files[:,1]==horse_ids[split_num])[0]
+		# np.array(list(pos_idx_split[split_num]) + list(neg_idx_split[split_num]))
+		idx_all = [idx_train, idx_test]
+		out_files = [os.path.join(out_dir_split,'train_'+str(split_num)+'.txt'),os.path.join(out_dir_split,'test_'+str(split_num)+'.txt')]
+		for idx_curr, out_file_curr in zip(idx_all,out_files):
+			annos = kept_files[idx_curr,:]
+			assert np.all(annos[:,-1]==annos[:,-2])
+			lines_all = [os.path.join(im_dir,str(anno_curr[0])+'.jpg')+' '+str(anno_curr[-1]) for anno_curr in annos]
+			print len(lines_all),out_file_curr,lines_all[0]
+			util.writeFile(out_file_curr,lines_all)
 
 def make_train_test_splits():
 	data_dir_meta ='../data/horse_51'
@@ -93,8 +134,24 @@ def save_h5py():
 
 
 def main():
-	save_h5py()
+	# make_train_test_splits_horse_based()
+	dir_server = '/disk3'
 
+	im_dir =os.path.join(dir_server,'maheen_data','eccv_18','data/horse_51')
+	data_dir_meta = '../data/horse_51'
+	out_dir_split = os.path.join(data_dir_meta,'train_test_split_horse_based')
+	for split_num in range(6):
+
+		test_file = os.path.join(out_dir_split,'test_'+str(split_num)+'.txt')
+		out_file_html = test_file[:test_file.rindex('.')]+'.html'
+		ims = [line_curr.split(' ')[0] for line_curr in util.readLinesFromFile(test_file)]
+		ims = [im.replace(data_dir_meta,im_dir) for im in ims]
+		print ims[0]
+		ims = [util.getRelPath(im,dir_server) for im in ims]
+		# print ims
+		visualize.writeHTML(out_file_html,[ims],[ims],224,224)
+		print out_file_html
+	
 	
 	
 
--- code/train_alexnet.py
+++ code/train_alexnet.py
@@ -1,3 +1,8 @@
+from helpers import util, visualize
+import random
+import scipy.misc
+from PIL import Image
+
 import torch.utils
 import torch.nn as nn
 import torch.optim as optim
@@ -10,46 +15,307 @@ import matplotlib.pyplot as plt
 import time
 import os
 import h5py
+import itertools
+
 
 def get_alexnet():
-	model = mode
+    model_ft = models.alexnet(pretrained=True)
+    new_classifier = list(model_ft.classifier.children())
+    new_classifier.pop()
+    new_layer = nn.Linear(4096,2)
+    torch.nn.init.xavier_normal(list(new_layer.parameters())[0])
+    torch.nn.init.constant(list(new_layer.parameters())[1],0.)
+    new_classifier.append(new_layer)
+    model_ft.classifier = nn.Sequential(*new_classifier)
+    return model_ft
+
+def get_new_batch(train_files_iterator, data_transformer, batch_size):
+    all_data = []
+    labels = []
+    for im_num in range(batch_size): 
+        train_file_curr = train_files_iterator.next()
+        train_file_curr,label = train_file_curr.split(' ')
+        label = int(label)
+        data_curr = data_transformer(Image.open(train_file_curr))
+        # Image.open(train_file_curr)
+        # data_transformer(Image.open(train_file_curr))
+        # print data_curr.shape,torch.min(data_curr), torch.max(data_curr)
+        # data_curr = scipy.misc.imresize(data_curr,(im_size,im_size))
+
+        all_data.append(data_curr)
+        labels.append(label)
+
+    all_data = torch.stack(all_data,dim=0)
+    labels = np.array(labels,dtype=int)
+    return all_data,labels
+
+def get_class_weights(train_files):
+    classes = [int(line_curr.split(' ')[1]) for line_curr in train_files]
+    counts = np.array([classes.count(0),classes.count(1)])
+    print counts
+    counts = counts/float(np.sum(counts))
+    counts = 1./counts
+    counts = counts/float(np.sum(counts))
+    print counts
+    return counts
+
+
+def train_model(out_dir_train,
+                train_file,
+                test_file,
+                data_transforms,
+                batch_size = None,
+                batch_size_val =None,
+                num_epochs = 100,
+                save_after = 20,
+                disp_after = 1,
+                plot_after = 10,
+                test_after = 1,
+                lr = 0.0001,dec_after = 100):
+
+    util.mkdir(out_dir_train)
+    log_file = os.path.join(out_dir_train,'log.txt')
+    plot_file = os.path.join(out_dir_train,'loss.jpg')
+    log_arr = []
+    plot_arr = [[],[]]
+    plot_val_arr = [[],[]]
+
+    
+
+    train_files = util.readLinesFromFile(train_file)
+    test_files = util.readLinesFromFile(test_file)
+    print len(test_files)
+    
+    if batch_size is None:
+        batch_size = len(train_files)
+
+    if batch_size_val is None:
+        batch_size_val = len(test_files)
+
+
+    class_weights = get_class_weights(train_files)
+    # print class_weights
+    
+    epoch_size = len(train_files)/batch_size
+    num_iterations = epoch_size*num_epochs
+    save_after = save_after*epoch_size
+    test_after = test_after* epoch_size
+
+
+
+    print 'epoch_size',epoch_size, 'num_iterations', num_iterations, 'batch_size',batch_size, 'batch_size_val',batch_size_val
+
+    torch.cuda.device(0)
+    iter_begin = 0
+    model = get_alexnet().cuda()
+    model.train(True)
+    criterion = nn.CrossEntropyLoss(weight = torch.FloatTensor(class_weights).cuda())
+    
+    optimizer = optim.SGD([{'params': model.features.parameters(), 'lr': lr[0]}]
+            +[{'params': model.classifier[index].parameters(), 'lr': lr[1]} for index in range(len(model.classifier)-1)]
+            +[{'params': model.classifier[-1].parameters(), 'lr': lr[2]}]
+            , lr=0, momentum=0.9)
+
+    if dec_after is not None:
+        dec_after = dec_after* epoch_size
+        exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=dec_after, gamma=0.1)
+
+    random.shuffle(train_files)
+    train_files_iterator = itertools.cycle(train_files)    
+    
+    test_files_iterator = itertools.cycle(test_files)    
+    
+
+    for num_iter in range(iter_begin,num_iterations):
+        if dec_after is not None:
+            exp_lr_scheduler.step()
+        
+        data, labels = get_new_batch(train_files_iterator, data_transforms['train'],batch_size)        
+        data = Variable(data.cuda())
+        labels = Variable(torch.LongTensor(labels).cuda())
+        optimizer.zero_grad()
+        loss = criterion(model(data), labels)    
+        loss_iter = loss.data[0]
+        loss.backward()
+        optimizer.step()
+        
+        plot_arr[0].append(num_iter); plot_arr[1].append(loss_iter)
+        str_display = 'lr: %.6f, iter: %d, loss: %.4f' %(optimizer.param_groups[-1]['lr'],num_iter,loss_iter)
+        log_arr.append(str_display)
+        print str_display
+
+        if num_iter % test_after == 0 :
+            model.eval()
+            data, labels = get_new_batch(test_files_iterator, data_transforms['val'],batch_size_val)        
+            # print data.shape
+            data = Variable(data.cuda())
+            labels = Variable(torch.LongTensor(labels).cuda())
+            loss = criterion(model(data), labels)    
+            loss_iter = loss.data[0]
+            plot_val_arr[0].append(num_iter); plot_val_arr[1].append(loss_iter)
+            str_display = 'lr: %.6f, iter: %d, val loss: %.4f' %(optimizer.param_groups[-1]['lr'],num_iter,loss_iter)
+            log_arr.append(str_display)
+            print str_display
+            model.train(True)
+            
+
+        if num_iter % epoch_size == 0 and num_iter>0:
+            print 'shuffling'
+            random.shuffle(train_files)
+            train_files_iterator = itertools.cycle(train_files)    
+
+        if (num_iter % plot_after== 0 and num_iter>0) or num_iter==num_iterations-1:
+            util.writeFile(log_file, log_arr)
+            if len(plot_val_arr[0])==0:
+                visualize.plotSimple([(plot_arr[0],plot_arr[1])],out_file = plot_file,title = 'Loss',xlabel = 'Iteration',ylabel = 'Loss',legend_entries=['Train'])
+            else:
+                visualize.plotSimple([(plot_arr[0],plot_arr[1]),(plot_val_arr[0],plot_val_arr[1])],out_file = plot_file,title = 'Loss',xlabel = 'Iteration',ylabel = 'Loss',legend_entries=['Train','Val'])
+
+        if num_iter % save_after == 0 or num_iter==num_iterations-1:
+            out_file = os.path.join(out_dir_train,'model_'+str(num_iter)+'.pt')
+            print 'saving',out_file
+            torch.save(model,out_file)
+            # save here
+
+def test_model(out_dir_train,
+                model_num,
+                train_file,
+                test_file,
+                data_transforms,
+                batch_size_val =None,):
+
+    out_dir_results = os.path.join(out_dir_train,'results_model_'+str(model_num))
+    util.mkdir(out_dir_results)
+    model_file = os.path.join(out_dir_train,'model_'+str(model_num)+'.pt')
+    log_file = os.path.join(out_dir_results,'log.txt')
+    log_arr=[]
+
+    test_files = util.readLinesFromFile(test_file)
+    print len(test_files)
+    
+    if batch_size_val is None:
+        batch_size_val = len(test_files)
+    
+    epoch_size = len(test_files)/batch_size_val
+    num_iterations = epoch_size
+
+
+    print 'epoch_size',epoch_size, 'num_iterations', num_iterations
+
+    torch.cuda.device(0)
+    iter_begin = 0
+    model = torch.load(model_file)
+    model.cuda()
+    model.eval()
+    
+    # train_files = util.readLinesFromFile(train_file)
+    # class_weights = get_class_weights(test_files)
+    
+    criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.SGD(model.classifier[-1].parameters(), lr=lr, momentum=0.9)
+
+    test_files_iterator = itertools.cycle(test_files)    
+    
+    predictions = []
+    labels_all = []
+
+    for num_iter in range(iter_begin,num_iterations):
+        
+        data, labels = get_new_batch(test_files_iterator, data_transforms['val'],batch_size_val)        
+        labels_all.append(labels)
+
+        data = Variable(data.cuda())
+        labels = Variable(torch.LongTensor(labels).cuda())
+        output = model(data)
+        predictions.append(np.argmax(output.data.cpu().numpy(),1))
+    
+        loss = criterion(output, labels)    
+        loss_iter = loss.data[0]
+        # plot_val_arr[0].append(num_iter); plot_val_arr[1].append(loss_iter)
+        str_display = 'iter: %d, val loss: %.4f' %(num_iter,loss_iter)
+        log_arr.append(str_display)
+        print str_display
+        
+
+        util.writeFile(log_file, log_arr)
+        
+    predictions = np.concatenate(predictions)[:len(test_files)]
+    labels_all = np.concatenate(labels_all)[:len(test_files)]
+    
+    accuracy = np.sum(predictions==labels_all)/float(labels_all.size)
+    str_display = 'accuracy: %.4f' %(accuracy)
+    print str_display
+    log_arr.append(str_display)
+    
+    util.writeFile(log_file, log_arr)
+    
 
 def main():
-	data_transforms = {
-	    'train': transforms.Compose([
-	        transforms.RandomSizedCrop(224),
-	        transforms.RandomHorizontalFlip(),
-	        transforms.ToTensor(),
-	        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-	    ]),
-	    'val': transforms.Compose([
-	        transforms.Scale(256),
-	        transforms.CenterCrop(224),
-	        transforms.ToTensor(),
-	        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-	    ]),
-	}
 
-	model_ft = models.alexnet(pretrained=True)
-	new_classifier = list(model_ft.classifier.children())
-	new_classifier.pop()
-	new_classifier.append(nn.Linear(4096, 2))
-	model_ft.classifier = nn.Sequential(*new_classifier)
 
-	criterion = nn.CrossEntropyLoss()
+    data_dir_meta = '../data/horse_51'
+    split_dir = os.path.join(data_dir_meta,'train_test_split')
+    num_splits = 5
 
-	# # Observe that all parameters are being optimized
-	optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    # split_dir = os.path.join(data_dir_meta,'train_test_split_horse_based')
+    # num_splits = 6
+    # visualize.writeHTMLForFolder(data_dir_meta)
+    # return
 
-	# # Decay LR by a factor of 0.1 every 7 epochs
-	exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=10, gamma=0.1)
+    num_epochs = 20
+    save_after = 20
+    disp_after = 1
+    plot_after = 10
+    test_after = 1
+    lr = [0, 0.00001,0.0001]
+    dec_after = 50
 
-	data_files = ['../data/horse_51/train_test_split/train_0.h5','../data/horse_51/train_test_split/test_0.h5']
+    out_dir_train_meta = '../experiments/alexnet_'+'_'.join([str(val) for val in [num_epochs,dec_after]+lr])
+    util.mkdir(out_dir_train_meta)
+    for split_num in range(num_splits):
+        out_dir_train = os.path.join(out_dir_train_meta,'split_'+str(split_num))
+        train_file = os.path.join(split_dir,'train_'+str(split_num)+'.txt')
+        val_file = os.path.join(split_dir,'test_'+str(split_num)+'.txt')
+        test_file = os.path.join(split_dir,'test_'+str(split_num)+'.txt')
 
-	file    = h5py.File(train_data_file, 'r')   # 'r' means that hdf5 file is open in read-only mode
-	train_data = torch.utils.data.DataLoader(zip(file['data'],file['labels']),batchsize=4)
-	
+        data_transforms = {
+            'train': transforms.Compose([
+                transforms.Scale([256,256]),
+                transforms.RandomSizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            'val': transforms.Compose([
+                transforms.Scale([224,224]),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+        }
+
+        
+
+        train_model(out_dir_train,
+                    train_file,
+                    test_file,
+                    data_transforms,
+                    num_epochs = num_epochs,
+                    save_after = save_after,
+                    disp_after = disp_after,
+                    plot_after = plot_after,
+                    test_after = test_after,
+                    lr = lr,
+                    dec_after = dec_after)
+
+        test_model(out_dir_train,
+                    num_epochs-1,
+                    train_file,
+                    test_file,
+                    data_transforms)
+        # break
+
+    
 
 
 if __name__=='__main__':
-	main()
+    main()
